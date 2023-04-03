ab0f4e3ddac58653ceb364f38ec783b4149e237f
Alexandre-Gregoire <114668095+Alexandre-Gregoire@users.noreply.github.com>
2023-04-03 21:07:25 +0200

    aplications matrices

---
+++ 5TTI_Matrices_Gregoire/5TTI_Matrices_Gregoire/MethodesDuProjet.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5TTI_Matrices_Gregoire
+{
+    public struct MethodesDuProjet
+    {
+        public void GenerationTableau(int nbLigne, int nbColonne,out int[,] MatricesAlea)
+        {
+            var Alea = new Random();
+            MatricesAlea = new int[nbLigne, nbColonne];
+            for (int ligne = 0; ligne < nbLigne; ligne++)
+            {
+                for (int colonne = 0; colonne < nbColonne; colonne++)
+                {
+                    MatricesAlea[ligne, colonne] = Alea.Next(0, 20);
+                }
+            }
+        }
+        public void stringMatrices(int[,] Matrices, out string stringTab)
+        {
+            stringTab = "";
+            for (int ligne = 0; ligne < Matrices.GetLength(0); ligne++)
+            {
+                for (int colonne = 0; colonne < Matrices.GetLength(1); colonne++)
+                {
+                    if (Matrices[ligne, colonne] < 10)
+                    {
+                        stringTab += Matrices[ligne, colonne] + " |";
+                    }
+                    else
+                    {
+                        stringTab += Matrices[ligne, colonne] + "|";
+                    }
+                    
+
+                }
+                stringTab += "\n";
+            }
+        }
+        public void Addition_Matrices(int[,] Matrices1, int[,] Matrices2,out int[,] Matrices3)
+        {
+            Matrices3 = new int[Matrices1.GetLength(0), Matrices1.GetLength(1)]; 
+            for (int ligne = 0; ligne < Matrices1.GetLength(0); ligne++)
+            {
+                for (int colonne = 0; colonne < Matrices1.GetLength(1); colonne++)
+                {
+                    Matrices3[ligne, colonne] = Matrices1[ligne, colonne] + Matrices2[ligne, colonne];
+                }
+            }
+        }
+        public void MultiplicationsMatrice(int[,] Matrices1, int[,] Matrices2,out int[,] matriceMulti)
+        {
+            matriceMulti = new int[Matrices1.GetLength(1), Matrices2.GetLength(0)];
+            for (int ligne = 0; ligne < Matrices2.GetLength(1); ligne++)
+            {
+                for (int colonne = 0; colonne < Matrices2.GetLength(1); colonne++)
+                {
+                    matriceMulti[ligne, colonne] = 0;
+                    for (int i = 0; i < Matrices1.GetLength(1); i++)
+                    {
+                        matriceMulti[ligne, colonne] += Matrices1[ligne, i] * Matrices2[i, colonne];
+                    }
+                }
+            }
+        }
+        public void LireReel(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention ! vous devez taper un nombre réel !");
+                Console.Write(question);
+                nUser = Console.ReadLine();
+            }
+
+        }
+        public void CreationTab(out int[,] Mat)
+        {
+            int nbLi;
+            int nbCol;
+
+            LireReel("Combiens voulez vous de lignes ?", out nbLi);
+            Console.WriteLine();
+            LireReel("Combiens voulez vous de colonne ?", out nbCol);
+            Console.WriteLine();
+            GenerationTableau(nbLi, nbCol, out Mat);
+
+        }
+    }
+}
---
+++ 5TTI_Matrices_Gregoire/5TTI_Matrices_Gregoire/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace _5TTI_Matrices_Gregoire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] Mat1;
+            int[,] Mat2;
+            int[,] Mat3;
+            string Reponse;
+            MethodesDuProjet mesOutils = new MethodesDuProjet();
+            string choix;
+            Console.WriteLine("Bienvenue dans ce programme d'adiotion et multiplications de matrices");
+            Console.WriteLine("Que voulez vous faire ? \n1. generer une matrice aleatoire \n2. aditionner 2 tableaux generer aléatoirement \n3. mutliplier 2 tableaux generer aléatoirement");
+            choix = Console.ReadLine();
+            if (choix == "1")
+            {
+                mesOutils.CreationTab(out Mat1);
+                mesOutils.stringMatrices(Mat1, out Reponse);
+                Console.WriteLine(Reponse);
+            }
+            else if (choix == "2")
+            {
+                Console.WriteLine("Tableaux 1 :");
+                mesOutils.CreationTab(out Mat1);
+                mesOutils.stringMatrices(Mat1, out Reponse);
+                Console.WriteLine(Reponse);
+                Console.WriteLine("Tableaux 2 :");
+                mesOutils.CreationTab(out Mat2);
+                mesOutils.stringMatrices(Mat2, out Reponse);
+                Console.WriteLine(Reponse + "-------------------------------");
+                mesOutils.Addition_Matrices(Mat1, Mat2, out Mat3);
+                mesOutils.stringMatrices(Mat3, out Reponse);
+                Console.WriteLine(Reponse);
+            }
+            else if (choix == "3")
+            {
+                Console.WriteLine("Tableaux 1 :");
+                mesOutils.CreationTab(out Mat1);
+                mesOutils.stringMatrices(Mat1, out Reponse);
+                Console.WriteLine(Reponse);
+                Console.WriteLine("Tableaux 2 :");
+                mesOutils.CreationTab(out Mat2);
+                mesOutils.stringMatrices(Mat2, out Reponse);
+                Console.WriteLine(Reponse + "-------------------------------");
+
+                mesOutils.MultiplicationsMatrice(Mat1, Mat2, out Mat3);
+                mesOutils.stringMatrices(Mat3, out Reponse);
+                Console.WriteLine(Reponse);
+            }
+            else
+            {
+
+            }
+
+
+
+
+
+        }
+       
+    }
+}
